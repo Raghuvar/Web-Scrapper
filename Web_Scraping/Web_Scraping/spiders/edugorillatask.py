21572eb060ac17a5e19e8162af80c3967b01f758
Raghuvar11 <201351003@iiitvadodara.ac.in>
2017-07-14 15:46:19 +0530

    Updating the task file

---
+++ Web_Scraping/Web_Scraping/spiders/edugorillatask.py
@@ -0,0 +1,18 @@
+"""
+	A python script: Used for extracting useful information availble on the websites.
+	@Author: Raghuvar Prajapati 
+		 (Undergrad in B.Tech(CSE) from IIIT-Vadodara)
+	
+ 
+"""
+
+import re
+import scrapy
+from scrapy.exceptions import CloseSpider
+
+
+class edugorillatask(scrapy.Spider):
+	name = ''
+	allowed domains = [""]
+	start_urls = []
+	start_urls.extend = [""]
