00176ef8d67f408fc794bf847d7e632e5ef12832
Raghuvar11 <201351003@iiitvadodara.ac.in>
2017-07-15 22:40:32 +0530

    Adding modified files

--- Web_Scraping/Web_Scraping/spiders/edugorillatask.py
+++ Web_Scraping/Web_Scraping/spiders/edugorillatask.py
@@ -1,10 +1,11 @@
 """
-	A python script: Used for extracting useful information availble on the websites.
-	@Author: Raghuvar Prajapati 	 
+	A python script: Used for extracting and browsing useful information availble on the websites.
+	@Author: Raghuvar Prajapati
+
 """
 
+
 import re
-#import csv 			 
 import scrapy
 from scrapy.exceptions import CloseSpider
 
@@ -17,8 +18,9 @@ class edugorillataskSpider(scrapy.Spider):
 
 	"""
 	Parse function is the most important function in any of the web scrapping task. Here will define a parse method
-	which will parse all the contents inside the elements of a h3 tag, a tag and href tag.
+	which will parse all the contents inside the elements of a h3 tag, 'a' tag and href tag.
 	"""
+	#
 	def parse(self, response):	
 		url_list = response.xpath('//h3/a/@href').extract();
 		for link in url_list:
@@ -26,23 +28,24 @@ class edugorillataskSpider(scrapy.Spider):
 			yield scrapy.Request(complete_url,callback=self.parseDetails)
 
 	"""
-	ParseDetails function will first select the data using css-selectors  
+	ParseDetails function will first select the data using css-selectors. So, response.xpath will return a
+	SelectorList. 
+	XPath is a language for selecting nodes in XML documents, which can also be used with HTML. 
 	"""
 	def parseDetails(self, response):
 		try:
 			details = response.xpath('//div/b/font/text()').extract()
 			email = self.extractSubString(details[6],'Email:', ', ');
 
-			#yield only if it(email) is a valid email address
-			if(self.validateEmail):
-				yield {'Name': details[0], 'Email-Id':email}
+			#get the email address of the schools if it's a valid email address
+			if(self.validateEmail(email)):
+				yield {'Name': details[0], 'Email-id':email}
 
-		# raise is a keyword which is used to get the errors as an exceptions.
+		# raise is a keyword which is used to get the errors as an exceptions in python. Throwing an error.
 		# Printing a fancy error message if can't parse the link.
 		except IndexError:
 			raise CloseSpider("reason=Canceled, Can not parse one of the links, Please refine your search")
 
-		#csvfile = "/home/raghuvar/Web-Scrapper/Web_Scraping/spiders/"
 	
 	"""
 	Function "extractSubString" will extract a particular substring from the input string
@@ -52,16 +55,10 @@ class edugorillataskSpider(scrapy.Spider):
 	def extractSubString(self,inputStr,start,end):
 		return inputStr[inputStr.find(start)+len(start):inputStr.rfind(end)]
 
-	"""	
-	with open(csvfile, "w") as output:
-		writer = csv.writer(output, lineterminator='\n')
-    	writer.writerow([yield])			
-	"""
 
-	# To check the correct format of the email available on the website
-	def validateEmail(email):
-		if len(email) > 8:
-			if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
-				return 1
-			return 0
+	# To check the correct format of the email of any of the schools listing on the link.
+	def validateEmail(self, email):
+		if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
+			return 1
+		return 0
 
